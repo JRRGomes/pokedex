b2e728b07fac8402a5d545efd708d1dce18655af
JRRGomes <jeffersonrolim@gmail.com>
2022-04-22 10:57:16 -0300

    feat: Created theme file with initial styles for the project

---
+++ src/theme.js
@@ -0,0 +1,27 @@
+const titleFont = "'Press Start 2P', cursive";
+
+const primaryFont = "'Roboto', sans-serif";
+
+const colors = {
+  backgroundColor: '#E6E6E6',
+};
+
+const sizes = {
+  xxsmall: '0.25rem',
+  xsmall: '0.5rem',
+  small: '0.75rem',
+  regular: '1rem',
+  medium: '1.5rem',
+  large: '2rem',
+  xlarge: '3rem',
+  xxlarge: '4rem',
+}
+
+const theme = {
+    titleFont,
+    primaryFont,
+    colors,
+    sizes,
+}
+
+export default theme;
