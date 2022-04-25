3e45544dd741caebafa4807c2ef98766767fb6fd
JRRGomes <jeffersonrolim@gmail.com>
2022-04-25 16:30:31 -0300

    feat: Changed the api request to use pokemon.names instead of using pokemon id

--- src/components/fetchPokemons.js
+++ src/components/fetchPokemons.js
@@ -3,13 +3,12 @@ import { api } from "../api";
 const TOTAL_POKEMONS = 1126;
 
 export const fetchPokemons = () => {
-  return api.get(`/pokemon/?offset=0&limit=20`)
+  return api.get(`/pokemon/?offset=0&limit=${TOTAL_POKEMONS}`)
   .then((pokemonsObj) => {
     const pokemonsList = pokemonsObj.data.results
-    return pokemonsList.map((pokemonData) => {
-      const pokemonId = pokemonData.url.split('/')[6]
-      return api.get(`/pokemon/${pokemonId}`)
+    return pokemonsList.map((pokemonData) => 
+       api.get(`/pokemon/${pokemonData.name}`)
       .then((pokemon) => pokemon.data)
-    })
+    )
   }).then((pokemons) => Promise.all(pokemons))
 }
