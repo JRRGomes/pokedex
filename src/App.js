bfe0182ea7480a3c68ed11d6b8885883c729a80c
JRRGomes <jeffersonrolim@gmail.com>
2022-04-27 16:18:06 -0300

    feat: Created use states to handle the catch error. Created render to set a loading and a error message

--- src/App.js
+++ src/App.js
@@ -8,19 +8,37 @@ import { fetchPokemons } from './services/pokemon';
 function App() {
 
   const [pokemons, setPokemons] = useState([])
+  const [hasError, setHasError] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
 
-  useEffect(() => {
-    fetchPokemons()
-    .then((pokemonsObj) => setPokemons(pokemonsObj))
+  const getPokemon = async () => {
+    try {
+      setIsLoading(true)
+      const pokemonsObj = await fetchPokemons()
+      setIsLoading(false)
+      setPokemons(pokemonsObj)
+    } catch (error) {
+      console.error(error)
+      setIsLoading(false)
+      setHasError(true)
+    }
+  }
+
+  useEffect( () => {
+    getPokemon()
   }, [])
 
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Heading variant='h3' align='center'>Pokedex</Heading>
-      {pokemons.map((pokemon) => (
-        <Heading key={pokemon.name} variant='h4' align='center'>{pokemon.name}</Heading>
-      ))}
+      {!hasError && !isLoading && <div>
+        {pokemons.map((pokemon) => (
+          <Heading key={pokemon.name} variant='h4' align='center'>{pokemon.name}</Heading>
+        ))}
+      </div>}
+      {hasError && <div>Could not load posts</div>}
+      {isLoading && <div>Loading pokemons...</div>}
     </ThemeProvider>
   );
 }
